69124ab454627c872465e0592cc2a93475eaa8d0
Juan <jjbachiller@gmail.com>
2020-11-21 23:50:19 +0100

    moving the video modal from the home page to the layout, so it's available on any page

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -59,6 +59,30 @@ License: You must have a valid license purchased only from themeforest(the above
 
         @include('modal.show_alert')
 
+
+        <!-- Modal -->
+
+        <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+
+
+              <div class="modal-body">
+
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <!-- 16:9 aspect ratio -->
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/OlUIEWUaB2s" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
         {{-- <script>var HOST_URL = "{{ route('quick-search') }}";</script> --}}
 
         {{-- Global Config (global config for global JS scripts) --}}
